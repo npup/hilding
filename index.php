7ec9b19c92c38cd2aa26f07ca11fc79e40791d86
P. Envall <petter.envall@gmail.com>
2012-05-28 16:46:58 +0200

    MOre

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-hej! jag är en Hilding
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Hilding Envall</title>
+</head>
+
+<body>
+<h1>Hilding Envall</h1>
+
+<p>
+
+... -> 
+  
+  
+</p>
+
+</body>
+</html>
